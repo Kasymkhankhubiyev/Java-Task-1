1551855b176c64d8762d85910c6f3232c35253af
Kasymkhankhubiyev <55043545+Kasymkhankhubiyev@users.noreply.github.com>
2020-04-25 03:50:00 +0700

    Update Translator.java

--- Translator.java
+++ Translator.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class Translator {
-    public String language = "English";
-}
+//StringBuffer & StringBuilder - двойники, только первый для многопоточных проектов, а второй для однопоточных.
+
+abstract class Translator { //строим слова при помощи StringBuilder
+    abstract StringBuilder build(File Inputfile); //abstract т.к. у нас ООП, а значит переводы будут через одну
+}              // и ту же функцию, но с перееобозначенными реализациями. 
