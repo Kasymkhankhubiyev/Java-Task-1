daf3ae300cfdd89e3ff8eb20e24c48e6648785e0
Kasymkhankhubiyev <55043545+Kasymkhankhubiyev@users.noreply.github.com>
2020-04-25 03:34:45 +0700

    Update Translator.java

--- Translator.java
+++ Translator.java
@@ -1,4 +1,9 @@
 package com.company;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Translator {
+    public String language = "English";
 }
